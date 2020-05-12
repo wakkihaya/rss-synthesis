c865dab4072641650376d3ef190cc0675a90ce9c
wakkihaya <wakkihaya@gmail.com>
2020-05-12 15:57:34 +0900

    first-commit"

---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="feed">
+            <div class="feed-title">
+                Tech Crunch
+            </div>
+            <div class="play-button">
+                再生
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
